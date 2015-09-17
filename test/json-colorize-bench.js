8cbb80a0eb0c4481d900df0ad268f9b7da9c1864
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:23:58 -0700

    Initial commit

---
+++ test/json-colorize-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var json-colorize = require(__dirname + '/lighter-json-colorize')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/json-colorize-test.js
@@ -0,0 +1,26 @@
+var json-colorize = require('../lighter-json-colorize')
+var is = global.is || require('exam/lib/is')
+
+describe('json-colorize', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
